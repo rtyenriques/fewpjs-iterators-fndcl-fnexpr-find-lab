492f3d1e5333190cfd40c18ffff10786ff86187a
= <rtyenriques@gmail.com>
2022-01-05 00:47:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function superbowlWin(array) {
+  let newA = array.find(function(ob) {
+       if (ob.result === 'W') {
+        return ob
+       }
+    })
+    if (newA === undefined){ 
+    return undefined }
+    else {return newA.year}
+}
+
+// function superbowlWin(values, year) {
+//   let winYear = values.find(function(ob) {
+//     if ()
+//   }
+  
+//   )
+
+
+// }
